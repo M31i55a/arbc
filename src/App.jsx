3f257c11fcff9df610032f8b0e637ca72a4e8e1f
M31i55a <melysgioda34@gmail.com>
2025-02-15 21:42:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <h1 class="text-5xl font-bold underline text-white bg-black min-h-screen">
+    Hello world!
+  </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
